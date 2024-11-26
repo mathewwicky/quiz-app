6cdb20fc3ef8df7f70d1dafd56f57dfeea08874e
mathew <mathewwicky8@gmail.com>
2024-11-26 03:50:47 +0200

    implemented the static questions and answer buttons

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -10,6 +10,28 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Questions Screen");
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize
+            .min, // instead of taking of all the available vertiacal space it only takes the minimum vertical space as needed
+
+        children: [
+          Text('The Question.....'),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
   }
 }
