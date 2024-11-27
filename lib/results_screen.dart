c69601dbf973a5abea102e9d413113d04d06c248
mathew <mathewwicky8@gmail.com>
2024-11-27 05:48:09 +0200

    fixing the results sumary screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
+
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.choosenAnswers});
@@ -33,7 +36,8 @@ class ResultsScreen extends StatelessWidget {
             SizedBox(
               height: 40,
             ),
-            Text('List of answers and questions'),
+            QuestionsSummary(getSummaryData()),
+           SizedBox(height: 30,),
             TextButton(
               onPressed: () {},
               child: Text('Rest Test'),
